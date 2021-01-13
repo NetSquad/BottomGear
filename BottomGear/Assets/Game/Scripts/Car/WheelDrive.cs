2e77d00e080b6410883ab026a5846d96d39bece1
Aitor Simona Bouzas <simonaaitor@gmail.com>
2021-01-13 17:50:00 +0100

    Added car break (change direction easily) + drag override on air

--- BottomGear/Assets/Game/Scripts/Car/WheelDrive.cs
+++ BottomGear/Assets/Game/Scripts/Car/WheelDrive.cs
@@ -31,18 +31,20 @@ namespace BottomGear
 		[Tooltip("Maximum torque applied to the driving wheels")]
 		public float maxTorque = 300f;
 		[Tooltip("Maximum brake torque applied to the driving wheels")]
-		public float brakeTorque = 30000f;
+		public float handBrakeTorque = 30000f;
 
 		[Header("Controller")]
-		[Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s).")]
+		[Tooltip("The vehicle's speed when the physics engine can use different amount of sub-steps (in m/s). Not editable in Play mode")]
 		public float criticalSpeed = 5f;
 		[Tooltip("The vehicle's limit speed  (in m/s).")]
 		public float maxSpeed = 30;
 		[Tooltip("The vehicle's acceleration multiplier.")]
 		public float acceleration = 5.0f;
-		[Tooltip("Simulation sub-steps when the speed is below critical.")]
+		[Tooltip("The vehicle's brake acceleration multiplier. Higher values will make the car stop faster.")]
+		public float brakeAcceleration = 15.0f;
+		[Tooltip("Simulation sub-steps when the speed is below critical. Not editable in Play mode.")]
 		public int stepsBelow = 1;
-		[Tooltip("Simulation sub-steps when the speed is above critical.")]
+		[Tooltip("Simulation sub-steps when the speed is above critical. Not editable in Play mode")]
 		public int stepsAbove = 5;
 		[Tooltip("The vehicle's jump force multiplier.")]
 		public int jumpForce = 10000;
@@ -56,6 +58,8 @@ namespace BottomGear
 
 		[Tooltip("Speed at which the vehicle rotates in x and y axis.")]
 		public Vector3 rotationSpeed = new Vector3(0, 40, 0);
+		[Tooltip("The linear drag coefficient override when object is flying. 0 means no damping.")]
+		public float flyingLinearDrag = 0.0f;
 
 		// --- Main components ---
 		private PhotonView photonView;
@@ -67,14 +71,16 @@ namespace BottomGear
 
 		// --- Internal variables ---
 		bool lockDown = false;
-		int currentSteps = 1;
+		float linearDragBackup = 0.0f;
 
-		System.Diagnostics.Stopwatch watch;
+		// Uncomment this to profile 
+		//System.Diagnostics.Stopwatch watch;
 
 		struct Wheel
 		{
 			public WheelCollider collider;
 			public GameObject mesh;
+			public Transform mTransform;
 		}
 
 		// --- Private gameplay variables ---
@@ -85,10 +91,12 @@ namespace BottomGear
 
 		public void Awake()
 		{
-			mTransform = GetComponent<Transform>();
+			mTransform = transform;
 			photonView = GetComponent<PhotonView>();
 			rb = GetComponent<Rigidbody>();
-			watch = new System.Diagnostics.Stopwatch();
+
+			// Uncomment this to profile 
+			//watch = new System.Diagnostics.Stopwatch();
 		}
 
 		// Find all the WheelColliders down in the hierarchy.
@@ -118,7 +126,10 @@ namespace BottomGear
 					MeshRenderer mr = m_Wheels[i].collider.gameObject.GetComponentInChildren<MeshRenderer>();
 
 					if (mr)
+					{
 						m_Wheels[i].mesh = mr.gameObject;
+						m_Wheels[i].mTransform = m_Wheels[i].mesh.transform;
+					}
 					else
 						Debug.LogError("No wheel mesh found in wheel collider's subtree");
 				}
@@ -138,57 +149,59 @@ namespace BottomGear
 
 			// --- Initialize jump timer to given interval ---
 			jumpTimer = jumpInterval;
+
+			// --- Configure wheel physics simulation steps depending on critical speed ---
+			m_Wheels[0].collider.ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
+
+			// --- Save rigidbody's drag ---
+			linearDragBackup = rb.drag;
 		}
 
 		void Update()
 		{
-			watch.Start();
+			// Uncomment this and timer log at the end of this function to profile 
+			//watch.Start();
 
 			// --- Only update if this is the local player ---
 			if (!photonView.IsMine && Photon.Pun.PhotonNetwork.IsConnectedAndReady)
 				return;
 
-            // --- If speed is above critical increase simulation steps ---
-            //if (rb.velocity.magnitude > criticalSpeed)
-            //{
-                m_Wheels[0].collider.ConfigureVehicleSubsteps(criticalSpeed, stepsBelow, stepsAbove);
-            //}
-
-            // --- Obtain input ---
-            Vector2 inputDirection;
+			// --- Obtain input ---
+			Vector2 inputDirection;
 			inputDirection.x = Input.GetAxis("Horizontal");
 			inputDirection.y = Input.GetAxis("Vertical");
+
+			// --- If no acceleration, release lock ---
+			float inputRawY = Input.GetAxisRaw("Vertical");
 
 			float angle = maxAngle * inputDirection.x;
 			float torque = maxTorque * inputDirection.y;
 			Vector3 direction = mTransform.forward * acceleration * inputDirection.y;
 
-			// --- Limit car speed ---
-			if (rb.velocity.magnitude > maxSpeed)
+			if (inputRawY <= 0)
 			{
-				torque = 0;
-			}
-			else if (IsGrounded())
-				rb.AddForce(direction, ForceMode.Acceleration);
-
-			// --- If no acceleration, release lock ---
-			if (Input.GetAxisRaw("Vertical") <= 0)
 				lockDown = false;
 
-			// --- Car on air rotation ---
-			if (!IsGrounded())
-			{
-				Vector2 inputDirectionRot;
-				inputDirectionRot.y = inputDirection.x;
-				inputDirectionRot.x = inputDirection.y;
-
-				// --- If lock was activated and we are moving forward block down rotation ---
-				if (lockDown && inputDirectionRot.x > 0)
-					inputDirectionRot.x = 0;
-
-				Quaternion deltaRot = Quaternion.Euler(inputDirectionRot * rotationSpeed);
-				mTransform.rotation = Quaternion.Slerp(mTransform.rotation, mTransform.rotation * deltaRot, Time.deltaTime * 2.0f);
+				// --- Apply manual brake, we want to go backwards ---
+				if (inputRawY != 0 && mTransform.InverseTransformDirection(rb.velocity).z > 0)
+				{
+					rb.AddForce(mTransform.forward * brakeAcceleration * inputDirection.y, ForceMode.Acceleration);
+				}
 			}
+            else
+            {
+                // --- Apply manual brake, we want to go forward ---
+                if (mTransform.InverseTransformDirection(rb.velocity).z < 0)
+                {
+                    rb.AddForce(mTransform.forward * brakeAcceleration * inputDirection.y, ForceMode.Acceleration);
+                }
+            }
+
+            // --- Limit car speed ---
+            if (rb.velocity.magnitude >= maxSpeed)
+                torque = 0;
+            else if (rb.velocity.magnitude < maxSpeed && IsGrounded() && direction != Vector3.zero)
+				rb.AddForce(direction, ForceMode.Acceleration);
 
 			// ---Car jump-- -
 			if (IsGrounded() && jumpTimer >= jumpInterval && Input.GetButtonDown("Jump"))
@@ -200,21 +213,39 @@ namespace BottomGear
                 rb.AddForce(mTransform.up * jumpForce, ForceMode.Impulse);
                 jumpTimer = 0.0f;
             }
+			// --- Car jump timer ---
+			jumpTimer += Time.deltaTime;
 
 			// --- Keep carattached to ground surface ---
-			if(snapToGround)
+			if (snapToGround)
 				rb.AddForce(-mTransform.up * snapForce, ForceMode.Force);
 
+			// --- Car on air rotation ---
+			if (!IsGrounded())
+			{
+				rb.drag = flyingLinearDrag;
+
+				float temp = inputDirection.y;
+				inputDirection.y = inputDirection.x;
+				inputDirection.x = temp;
+
+				// --- If lock was activated and we are moving forward block down rotation ---
+				if (lockDown && inputDirection.x > 0)
+					inputDirection.x = 0;
+
+				Quaternion deltaRot = Quaternion.Euler(inputDirection * rotationSpeed);
+				mTransform.rotation = Quaternion.Slerp(mTransform.rotation, mTransform.rotation * deltaRot, Time.deltaTime * 2.0f);
+			}
+			else
+				rb.drag = linearDragBackup;
+			 
 			// --- Car flip ---
 			if (mTransform.up.y < -0.75)
             {
 				rb.MoveRotation(rb.rotation * Quaternion.Euler(0, 0, 180));
             }
 
-            // --- Car jump timer ---
-            jumpTimer += Time.deltaTime;
-
-			float handBrake = Input.GetButton("R2") ? brakeTorque : 0;
+			float handBrake = Input.GetButton("R2") ? handBrakeTorque : 0;
 
 			for (int i = 0; i < m_Wheels.Length; ++i)
 			{
@@ -227,30 +258,22 @@ namespace BottomGear
 
 				if (m_Wheels[i].mesh.name == "Wheel3Mesh"
 						|| m_Wheels[i].mesh.name == "Wheel4Mesh")
-				{
 					wheel.brakeTorque = handBrake;
-				}
-
+				
 				if (m_Wheels[i].mesh.name == "Wheel1Mesh"
 						|| m_Wheels[i].mesh.name == "Wheel2Mesh"
 						&& driveType != DriveType.FrontWheelDrive)
-				{
 					wheel.motorTorque = torque;
 					
-				}
-
 				if (m_Wheels[i].mesh.name == "Wheel1Mesh"
 						|| m_Wheels[i].mesh.name == "Wheel2Mesh"
 						&& driveType != DriveType.RearWheelDrive)
-				{
 					wheel.motorTorque = torque;
-				}
-
 			}
-
-			Debug.Log(watch.Elapsed.TotalMilliseconds);
-
-			watch.Reset();
+			 
+			// Uncomment this and timer start to profile 
+			//Debug.Log(watch.Elapsed.TotalMilliseconds);
+			//watch.Reset();
 
 			//Update the RTPC for the engine sound
 			float speed = rb.velocity.magnitude / maxSpeed;
@@ -259,6 +282,9 @@ namespace BottomGear
 
         private void LateUpdate()
         {
+			Quaternion q;
+			Vector3 p;
+
 			for (int i = 0; i < m_Wheels.Length; ++i)
 			{
 				ref WheelCollider wheel = ref m_Wheels[i].collider;
@@ -266,28 +292,26 @@ namespace BottomGear
 				// --- Update visual wheel's mesh --- 
 				if (m_Wheels[i].mesh)
 				{
-					Quaternion q;
-					Vector3 p;
 					wheel.GetWorldPose(out p, out q);
 
 					// --- Rotate wheel according to collider's rotation ---
-					Transform shapeTransform = m_Wheels[i].mesh.transform;
+					//Transform shapeTransform = m_Wheels[i].mesh.transform;
 
 					if (m_Wheels[i].mesh.name == "Wheel1Mesh"
 					 || m_Wheels[i].mesh.name == "Wheel3Mesh")
-						shapeTransform.position = p + transform.right * m_Wheels[i].collider.radius / 2.0f;
+						m_Wheels[i].mTransform.position = p + transform.right * m_Wheels[i].collider.radius / 2.0f;
 					else
-						shapeTransform.position = p - transform.right * m_Wheels[i].collider.radius / 2.0f;
+						m_Wheels[i].mTransform.position = p - transform.right * m_Wheels[i].collider.radius / 2.0f;
 
 
 					if (m_Wheels[i].mesh.name == "Wheel1Mesh"
                         || m_Wheels[i].mesh.name == "Wheel2Mesh")
-                    {						                        
-						shapeTransform.rotation = q*Quaternion.Euler(0,180, 0);
+                    {
+						m_Wheels[i].mTransform.rotation = q*Quaternion.Euler(0,180, 0);
 					}
 					else
                     {
-						shapeTransform.rotation = q;
+						m_Wheels[i].mTransform.rotation = q;
 					}
 				}
 			}
