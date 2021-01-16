0376cc06dd0437092f8d8e08d3a3e4a6a8f9fdf3
Aitor Simona Bouzas <simonaaitor@gmail.com>
2021-01-16 14:00:20 +0100

    cleaned camera transitions script

--- BottomGear/Assets/Game/Scripts/Car/CameraTransitions.cs
+++ BottomGear/Assets/Game/Scripts/Car/CameraTransitions.cs
@@ -20,20 +20,13 @@ public class CameraTransitions : MonoBehaviour
     private Cinemachine.CinemachineMixingCamera mixingCamera;
     private GameObject Parent;
 
-    public float minDistance = 5.0f;
-
     // Start is called before the first frame update
     void Start()
     {
         Parent = transform.parent.gameObject;
         CurrentCamera = FollowCamera;
-        //FollowCamera.SetActive(true);
-        //FollowCameraFar.SetActive(false);
-        //FollowCameraUp.SetActive(false);
-        //FrontCamera.SetActive(false);
         brain = MainCamera.GetComponent<Cinemachine.CinemachineBrain>();
         mixingCamera = GetComponent<Cinemachine.CinemachineMixingCamera>();
-        //ixingCamera.m_Weight0
     }
 
     // Update is called once per frame
@@ -43,27 +36,10 @@ public class CameraTransitions : MonoBehaviour
         if (brain.ActiveBlend != null && !brain.ActiveBlend.IsComplete)
             return;
 
-        
-
         float distanceToTarget = Mathf.Abs(Vector3.Distance(Parent.transform.position, CurrentCamera.transform.position));
 
-        //mixingCamera.m_Weight0 = 10 - distanceToTarget;
         mixingCamera.m_Weight2 = Mathf.Clamp(FollowFarCamInitialWeight - distanceToTarget, 0, FollowFarCamInitialWeight);
         mixingCamera.m_Weight3 = Mathf.Clamp(Mathf.Abs(Parent.transform.rotation.eulerAngles.GetXCorrectedEuler().x / 90 * FollowUpCamInitialWeight), 0, FollowUpCamInitialWeight);
-        //Debug.Log(Mathf.Abs(Vector3.Distance(transform.position, CurrentCamera.transform.position)));
-
-        //if (Mathf.Abs(Vector3.Distance(transform.position, CurrentCamera.transform.position)) < minDistance)
-        //{
-        //    CurrentCamera.SetActive(false);
-        //    CurrentCamera = FollowCameraFar;
-        //    CurrentCamera.SetActive(true);
-        //}
-        //else if (CurrentCamera == FollowCameraFar)
-        //{
-        //    CurrentCamera.SetActive(false);
-        //    CurrentCamera = FollowCamera;
-        //    CurrentCamera.SetActive(true);
-        //}
 
         // --- Follow - Rear ---
         if (Input.GetButton("Rear") || Input.GetMouseButton(2))
@@ -75,19 +51,14 @@ public class CameraTransitions : MonoBehaviour
             if (CurrentCamera != FrontCamera)
             {
                 mixingCamera.m_Weight1 = FrontCamInitialWeight;
-                //CurrentCamera.SetActive(false);
                 CurrentCamera = FrontCamera;
-                //CurrentCamera.SetActive(true);
             }
         }
         else if (CurrentCamera == FrontCamera)
         {
             mixingCamera.m_Weight1 = 0;
             mixingCamera.m_Weight0 = FollowCamInitialWeight;
-
-            //CurrentCamera.SetActive(false);
             CurrentCamera = FollowCamera;
-            //CurrentCamera.SetActive(true);
         }
     }
 }
