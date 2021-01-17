5e9fbff8d09e6a628bf611cbdc3ec86e2761af52
Aitor Simona Bouzas <simonaaitor@gmail.com>
2021-01-17 18:30:39 +0100

    Added VFX graph, snoe over level

---
+++ BottomGear/Assets/Cool Visual Effects - Part 1/Scripts/ConstantRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotation : MonoBehaviour
+{
+    public float rotationSpeed = 10.0f;
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ BottomGear/Assets/Cool Visual Effects - Part 1/Scripts/EffectManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class EffectManager : MonoBehaviour
+{
+    public GameObject[] effects;
+    private VisualEffect[] vfxs;
+    private int currentEffect = 0;
+
+    private void Start()
+    {
+        vfxs = new VisualEffect[effects.Length];
+        for (int i = 0; i < effects.Length; ++i)
+        {
+            effects[i].SetActive(false);
+            vfxs[i] = effects[i].GetComponent<VisualEffect>();
+        }
+
+        effects[currentEffect].SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(currentEffect - 1 >= 0)
+            {
+                vfxs[currentEffect].Stop();
+                effects[currentEffect].SetActive(false);
+                --currentEffect;
+                effects[currentEffect].SetActive(true);
+                vfxs[currentEffect].Play();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentEffect + 1 < effects.Length)
+            {
+                vfxs[currentEffect].Stop();
+                effects[currentEffect].SetActive(false);
+                ++currentEffect;
+                effects[currentEffect].SetActive(true);
+                vfxs[currentEffect].Play();
+            }
+        }
+    }
+}
